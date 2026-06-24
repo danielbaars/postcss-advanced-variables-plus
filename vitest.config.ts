243a20a1889090042634643275aa92f02b73d962
Daniel Baars <danielbaars@gmail.com>
2026-06-24 20:08:12 +0200

    ci: more tooling setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "html", "lcov"],
+      include: ["src/**"],
+      exclude: ["**/*.test.{ts,tsx}", "**/*.d.ts"],
+    },
   },
 });
